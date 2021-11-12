4c69daae1e2a648e10dc08990ca77a8dd40107ec
Justin Krometis <jkrometis@vt.edu>
2021-11-12 09:09:49 -0500

    try adding --num_jobs to align

--- mfa_align.sh
+++ mfa_align.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -N 1 --tasks-per-node=8 #8 because we sometimes need more memory
 #SBATCH -t 24:00:00
-#SBATCH -p normal_q
+#SBATCH -p largemem_q #normal_q
 #SBATCH -A katcarm_lab
 ##SBATCH --mail-user=katcarm@vt.edu
 #SBATCH --mail-type=END
@@ -12,7 +12,11 @@ module reset
 module load Anaconda3/2020.07
 module load tinkercliffs-rome/kaldi/20210429-foss-2020b
 
-export OPENBLAS_NUM_THREADS=$SLURM_NTASKS
+#give two cores' worth of memory per job
+export njobs=$(( $SLURM_NTASKS/2 ))
+#i don't know if this does anything but seems like it doesn't hurt to try
+export OPENBLAS_NUM_THREADS=2
+export OMP_NUM_THREADS=2
 
 #(mimic) initialize conda
 source conda_setup.sh
@@ -30,7 +34,7 @@ config="config_long.yml"
 echo "LOG: $( date ): Start mfa align"                           | tee -a $logfile
 
 #run the alignment
-mfa align --config_path=$config --clean $corpus $dict english aligned | tee -a $logfile
+mfa align --config_path=$config --clean --num_jobs $njobs $corpus $dict english aligned | tee -a $logfile
 
 #print some useful information at the end
 echo "Contents of corpus/align/log/align.final.0.log:"           | tee -a $logfile
